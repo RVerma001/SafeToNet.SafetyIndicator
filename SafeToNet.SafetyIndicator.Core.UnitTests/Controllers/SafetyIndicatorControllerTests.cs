ae97d0825d280798b29c2c2daec64d21d36b8ae8
RVerma001 <rverma@safetonet.com>
2019-10-25 13:50:33 +0100

    Added more coverage to controller tests.

--- SafeToNet.SafetyIndicator.Core.UnitTests/Controllers/SafetyIndicatorControllerTests.cs
+++ SafeToNet.SafetyIndicator.Core.UnitTests/Controllers/SafetyIndicatorControllerTests.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SafeToNet.SafetyIndicator.TestHelper;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace SafeToNet.SafetyIndicator.Core.UnitTests.Controllers
@@ -42,6 +43,73 @@ namespace SafeToNet.SafetyIndicator.Core.UnitTests.Controllers
             // Assert
             Assert.NotNull(result);
             Assert.Equal(expected, okResult.Value);
+        }
+
+        [Fact]
+        public void CreateInsights_Null_BadRequest()
+        {
+            // Arrange
+            var data = StaticData.NullInsights;
+            var controller = Mock.Controller;
+
+            // Act
+            var result = controller.CreateInsights(data);
+            var badResult = result.Result as BadRequestResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<BadRequestResult>(badResult);
+        }
+
+        [Fact]
+        public void CreateInsights_Null_OkRequest()
+        {
+            // Arrange
+            var data = StaticData.InsightsRequests;
+            var controller = Mock.Controller;
+
+            // Act
+            var result = controller.CreateInsights(data);
+            var okResult = result.Result as OkResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<OkResult>(okResult);
+        }
+
+        [Fact]
+        public void GetInsightsBetweenTimes_Null_BadRequest()
+        {
+            // Arrange
+            System.Guid data = System.Guid.Empty;
+            var controller = Mock.Controller;
+
+            // Act
+            var result = controller.GetInsightsBetweenTimes(data);
+            var badResult = result.Result as BadRequestResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<BadRequestResult>(badResult);
+        }
+
+        [Fact]
+        public async void GetInsightsBetweenTimes_Null_OkRequest()
+        {
+            // Arrange
+            var data = StaticData.DeviceID;
+            var controller = Mock.Controller;
+
+            // Act
+            var createResult = controller.CreateInsights(StaticData.InsightsRequests);
+
+            await createResult;
+
+            var result = controller.GetInsightsBetweenTimes(data);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(TaskStatus.RanToCompletion, result.Status);
         }
     }
 }
